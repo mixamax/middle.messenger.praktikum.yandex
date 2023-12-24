113cfdec5cab8156461948fe70185752af008fee
Mixamax <mixamax@inbox.ru>
2023-12-24 16:16:51 +0300

    add port

--- vite.congig.ts
+++ vite.congig.ts
@@ -1,3 +1,12 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({ build: { outDir: "dist" } });
+export default defineConfig({
+    build: { outDir: "dist" },
+    server: {
+        port: 3000,
+    },
+});
+
+// preview: {
+//     port: 3000,
+// },
