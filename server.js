a2fee4ad5d061aa5331739e29d42d867484ce2c9
Mixamax <mixamax@inbox.ru>
2024-02-05 18:08:00 +0300

    ch express

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 app.use(express.static("./dist"));
 
+app.get("/sign-up", function (req, res) {
+    res.send("hello world");
+});
+
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
