30d2e5c15ec76dfa51d66367b9dec7e7aed2da90
Mixamax <mixamax@inbox.ru>
2024-02-05 18:11:26 +0300

    ch expr2

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ app.use(express.static("./dist"));
 app.get("/sign-up", function (req, res) {
     res.send("hello world");
 });
+app.get("/settings", function (req, res) {
+    res.send("hello world");
+});
+app.get("/settings", function (req, res) {
+    res.send("hello world");
+});
+app.get("/messenger", function (req, res) {
+    res.send("hello world");
+});
+app.get("/404", function (req, res) {
+    res.send("hello world");
+});
+app.get("/500", function (req, res) {
+    res.send("hello world");
+});
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
