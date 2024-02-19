1d271920f905f4f9ed1930bc63d7a95218e86018
Mixamax <mixamax@inbox.ru>
2024-02-19 12:04:01 +0300

    add eslint & lint to precommit add env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 // const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("./dist"));
 
